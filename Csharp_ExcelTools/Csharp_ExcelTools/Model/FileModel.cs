05a5a5197aa4923eb81a96c09224a7cf34070ede
joelee2017 <33776374+joelee2017@users.noreply.github.com>
2022-06-28 10:54:35 +0800

    Update FileModel.cs

--- Csharp_ExcelTools/Csharp_ExcelTools/Model/FileModel.cs
+++ Csharp_ExcelTools/Csharp_ExcelTools/Model/FileModel.cs
@@ -1,5 +1,10 @@
-﻿public class FileModel
+public class FileModel
 {
+    /// <summary>
+    /// 檔案名稱
+    /// </summary>
+    public string FileName { get; set; }
+
     /// <summary>
     /// 檔案路徑
     /// </summary>
@@ -19,4 +24,31 @@
     /// 要包含的欄位
     /// </summary>
     public string[] CellVertical { get; set; }
+
+    /// <summary>
+    /// 檔案模版路徑
+    /// </summary>
+    public string TempleateFilePath { get; set; }
+
+    /// <summary>
+    /// 檔案匯出路徑 - 前
+    /// </summary>
+    public string ExportPath1 { get; set; }
+
+    /// <summary>
+    /// 檔案匯出路徑 - 中
+    /// </summary>
+    public string ExportPath2 { get; set; }
+
+
+    /// <summary>
+    /// 檔案匯出路徑 - 中
+    /// </summary>
+    public string ExportPath3 { get; set; }
+
+
+    /// <summary>
+    /// 檔案匯出路徑 - 後
+    /// </summary>
+    public string ExportPath4 { get; set; }
 }
